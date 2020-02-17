87f18b1d6de20fe66081de7649e42b078ca3f26d
LajosPolya <lajospolya94@gmail.com>
2020-02-17 13:12:43 -0500

    find methods through reflection at static initialization phase

--- src/main/java/com/lajospolya/spotifyapiwrapper/reflection/ReflectiveSpotifyClientService.java
+++ src/main/java/com/lajospolya/spotifyapiwrapper/reflection/ReflectiveSpotifyClientService.java
@@ -1,5 +1,6 @@
 package com.lajospolya.spotifyapiwrapper.reflection;
 
+import com.lajospolya.spotifyapiwrapper.spotifyexception.SpotifyBadClassDefinitionException;
 import com.lajospolya.spotifyapiwrapper.spotifyrequest.AbstractSpotifyRequest;
 
 import java.lang.reflect.*;
@@ -10,19 +11,31 @@ public class ReflectiveSpotifyClientService implements IReflectiveSpotifyClientS
     private static final String SET_ACCESS_TOKEN_METHOD_NAME = "reflectiveSetAccessToken";
     private static final String BUILD_REQUEST_METHOD_NAME = "reflectiveBuildRequest";
     private static final Class<?> ABSTRACT_SPOTIFY_REQUEST_CLASS = AbstractSpotifyRequest.class;
+    private static final Method setAccessTokenMethod;
+    private static final Method buildRequestMethod;
 
-    public <T> void setAccessTokenOfRequest(AbstractSpotifyRequest<T> spotifyRequest, String accessToken) throws IllegalAccessException, InvocationTargetException, NoSuchMethodException
+    static
     {
-        // Would it beneficial for this Method to be static? What should I do in the exceptional case in static block initialization
-        Method setAccessTokenMethod = ABSTRACT_SPOTIFY_REQUEST_CLASS.getDeclaredMethod(SET_ACCESS_TOKEN_METHOD_NAME, String.class);
+        try
+        {
+            setAccessTokenMethod = ABSTRACT_SPOTIFY_REQUEST_CLASS.getDeclaredMethod(SET_ACCESS_TOKEN_METHOD_NAME, String.class);
+            buildRequestMethod = ABSTRACT_SPOTIFY_REQUEST_CLASS.getDeclaredMethod(BUILD_REQUEST_METHOD_NAME, (Class<?>[]) null);
+        }
+        catch (NoSuchMethodException e)
+        {
+            throw new SpotifyBadClassDefinitionException(ABSTRACT_SPOTIFY_REQUEST_CLASS + " does not contain one of the methods needed to send a request");
+        }
+    }
+
+    public <T> void setAccessTokenOfRequest(AbstractSpotifyRequest<T> spotifyRequest, String accessToken) throws IllegalAccessException, InvocationTargetException
+    {
         setAccessTokenMethod.setAccessible(true);
         setAccessTokenMethod.invoke(spotifyRequest, accessToken);
         setAccessTokenMethod.setAccessible(false);
     }
 
-    public <T> HttpRequest buildRequest(AbstractSpotifyRequest<T> spotifyRequest) throws NoSuchMethodException, InvocationTargetException, IllegalAccessException
+    public <T> HttpRequest buildRequest(AbstractSpotifyRequest<T> spotifyRequest) throws InvocationTargetException, IllegalAccessException
     {
-        Method buildRequestMethod = ABSTRACT_SPOTIFY_REQUEST_CLASS.getDeclaredMethod(BUILD_REQUEST_METHOD_NAME, (Class<?>[]) null);
         buildRequestMethod.setAccessible(true);
         HttpRequest request =  (HttpRequest) buildRequestMethod.invoke(spotifyRequest, (Object[]) null);
         buildRequestMethod.setAccessible(false);
---
+++ src/main/java/com/lajospolya/spotifyapiwrapper/spotifyexception/SpotifyBadClassDefinitionException.java
@@ -0,0 +1,9 @@
+package com.lajospolya.spotifyapiwrapper.spotifyexception;
+
+public class SpotifyBadClassDefinitionException extends RuntimeException
+{
+    public SpotifyBadClassDefinitionException(String message)
+    {
+        super(message);
+    }
+}
