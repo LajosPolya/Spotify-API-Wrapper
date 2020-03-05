bdc07b24b66c2b614eef554df838bf1ed17561f0
LajosPolya <lajospolya94@gmail.com>
2020-03-04 21:43:04 -0500

    GET Me Tracks (get my saved tracks)

---
+++ src/main/java/com/lajospolya/spotifyapiwrapper/response/SavedTrack.java
@@ -0,0 +1,27 @@
+package com.lajospolya.spotifyapiwrapper.response;
+
+public class SavedTrack
+{
+    private String added_at;
+    private Track track;
+
+    public String getAdded_at()
+    {
+        return added_at;
+    }
+
+    public void setAdded_at(String added_at)
+    {
+        this.added_at = added_at;
+    }
+
+    public Track getTrack()
+    {
+        return track;
+    }
+
+    public void setTrack(Track track)
+    {
+        this.track = track;
+    }
+}
