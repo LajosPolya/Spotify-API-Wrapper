a5a07be1f2dc97841e22dda3852fdb831ead4c2f
LajosPolya <lajospolya94@gmail.com>
2020-03-04 21:19:09 -0500

    GET Me Following Contains (is user following an album)

--- src/main/java/com/lajospolya/spotifyapiwrapper/spotifyrequest/GetMeAlbumsContains.java
+++ src/main/java/com/lajospolya/spotifyapiwrapper/spotifyrequest/GetMeAlbumsContains.java
@@ -1,46 +1,42 @@
 package com.lajospolya.spotifyapiwrapper.spotifyrequest;
 
-import com.lajospolya.spotifyapiwrapper.enumeration.FollowType;
 import org.springframework.web.util.UriComponentsBuilder;
 
 import java.net.http.HttpRequest;
 import java.util.List;
 
-public class GetMeFollowingContains extends AbstractSpotifyRequest<List<Boolean>>
+public class GetMeAlbumsContains extends AbstractSpotifyRequest<List<Boolean>>
 {
-    private static final String REQUEST_URI_STRING = SPOTIFY_V1_API_URI +  "me/following/contains";
+    private static final String REQUEST_URI_STRING = SPOTIFY_V1_API_URI +  "me/albums/contains";
 
-    private GetMeFollowingContains(HttpRequest.Builder requestBuilder)
+    private GetMeAlbumsContains(HttpRequest.Builder requestBuilder)
     {
         super(requestBuilder);
     }
 
     public static class Builder extends AbstractBuilder
     {
-        private FollowType type;
         private List<String> ids;
 
-        public Builder(FollowType type, List<String> ids)
+        public Builder(List<String> ids)
         {
-            validateParametersNotNull(type, ids);
+            validateParametersNotNull(ids);
             spotifyRequestParamValidationService.validateFollowIds(ids);
-            this.type = type;
             this.ids = ids;
         }
 
-        public GetMeFollowingContains build()
+        public GetMeAlbumsContains build()
         {
             UriComponentsBuilder requestUriBuilder =  UriComponentsBuilder.fromUriString(REQUEST_URI_STRING);
 
             String commaSeparatedIds = String.join(",", this.ids);
 
             requestUriBuilder.queryParam(IDS_QUERY_PARAM, commaSeparatedIds);
-            requestUriBuilder.queryParam(TYPE_QUERY_PARAM, type.getName());
 
             HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
                     .uri(requestUriBuilder.build().toUri())
                     .GET();
-            return new GetMeFollowingContains(requestBuilder);
+            return new GetMeAlbumsContains(requestBuilder);
         }
     }
 }
