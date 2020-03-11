508765db33f9936329f0e5344b2be40b88e17e42
LajosPolya <lajospolya94@gmail.com>
2020-03-10 20:06:45 -0400

    GET Me Player Currently Playing (get currently playing track)7

---
+++ src/main/java/com/lajospolya/spotifyapiwrapper/spotifyrequest/GetMePlayerCurrentlyPlaying.java
@@ -0,0 +1,38 @@
+package com.lajospolya.spotifyapiwrapper.spotifyrequest;
+
+import org.springframework.web.util.UriComponentsBuilder;
+
+import java.net.http.HttpRequest;
+
+public class GetMePlayerCurrentlyPlaying extends AbstractSpotifyRequest<String>
+{
+    private static final String REQUEST_URI_STRING = SPOTIFY_V1_API_URI +  "me/player/currently-playing";
+
+    private GetMePlayerCurrentlyPlaying(HttpRequest.Builder requestBuilder)
+    {
+        super(requestBuilder);
+    }
+
+    public static class Builder extends AbstractBuilder
+    {
+        private String market;
+
+        public Builder(String market)
+        {
+            validateParametersNotNull(market);
+            this.market = market;
+        }
+
+        public GetMePlayerCurrentlyPlaying build()
+        {
+            UriComponentsBuilder requestUriBuilder =  UriComponentsBuilder.fromUriString(REQUEST_URI_STRING);
+
+            requestUriBuilder.queryParam(MARKET_QUERY_PARAM, this.market);
+
+            HttpRequest.Builder requestBuilder = HttpRequest.newBuilder()
+                    .uri(requestUriBuilder.build().toUri())
+                    .GET();
+            return new GetMePlayerCurrentlyPlaying(requestBuilder);
+        }
+    }
+}
