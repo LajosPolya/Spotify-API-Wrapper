67303d72169377f900dc40aeb4b02b34e3a64c17
LajosPolya <lajospolya94@gmail.com>
2020-03-22 16:34:11 -0400

    Complete DeviceType

--- src/main/java/com/lajospolya/spotifyapiwrapper/enumeration/DeviceType.java
+++ src/main/java/com/lajospolya/spotifyapiwrapper/enumeration/DeviceType.java
@@ -6,10 +6,30 @@ public enum DeviceType
 {
     @SerializedName("Computer")
     COMPUTER("Computer"),
+    @SerializedName("Tablet")
+    TABLET("Tablet"),
     @SerializedName("Smartphone")
     SMARTPHONE("Smartphone"),
     @SerializedName("Speaker")
-    SPEAKER("Speaker");
+    SPEAKER("Speaker"),
+    @SerializedName("TV")
+    TV("TV"),
+    @SerializedName("AVR")
+    AVR("AVR"),
+    @SerializedName("STB")
+    STB("STB"),
+    @SerializedName("AudioDongle")
+    AUDIODONGLE("AudioDongle"),
+    @SerializedName("GameConsole")
+    GAMECONSOLE("GameConsole"),
+    @SerializedName("CastVideo")
+    CASTVIDEO("CastVideo"),
+    @SerializedName("CastAudio")
+    CASTAUDIO("CastAudio"),
+    @SerializedName("Automobile")
+    AUTOMOBILE("Automobile"),
+    @SerializedName("Unknown")
+    UNKNOWN("Unknown");
 
     private String type;
 
