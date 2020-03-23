398efe5bd0bfaa7f1a9e71eeae2c7443404061b6
LajosPolya <lajospolya94@gmail.com>
2020-03-22 21:28:33 -0400

    CurrentlyPlaying object

---
+++ src/main/java/com/lajospolya/spotifyapiwrapper/response/CurrentlyPlaying.java
@@ -0,0 +1,84 @@
+package com.lajospolya.spotifyapiwrapper.response;
+
+import com.lajospolya.spotifyapiwrapper.enumeration.CurrentlyPlayingType;
+
+public class CurrentlyPlaying
+{
+    private Context context;
+    private Long timestamp;
+    private Integer progress_ms;
+    private Boolean is_playing;
+    private TrackOrEpisode item;
+    private CurrentlyPlayingType currently_playing_type;
+    private Actions actions;
+
+    public Context getContext()
+    {
+        return context;
+    }
+
+    public void setContext(Context context)
+    {
+        this.context = context;
+    }
+
+    public Long getTimestamp()
+    {
+        return timestamp;
+    }
+
+    public void setTimestamp(Long timestamp)
+    {
+        this.timestamp = timestamp;
+    }
+
+    public Integer getProgress_ms()
+    {
+        return progress_ms;
+    }
+
+    public void setProgress_ms(Integer progress_ms)
+    {
+        this.progress_ms = progress_ms;
+    }
+
+    public Boolean getIs_playing()
+    {
+        return is_playing;
+    }
+
+    public void setIs_playing(Boolean is_playing)
+    {
+        this.is_playing = is_playing;
+    }
+
+    public TrackOrEpisode getItem()
+    {
+        return item;
+    }
+
+    public void setItem(TrackOrEpisode item)
+    {
+        this.item = item;
+    }
+
+    public CurrentlyPlayingType getCurrently_playing_type()
+    {
+        return currently_playing_type;
+    }
+
+    public void setCurrently_playing_type(CurrentlyPlayingType currently_playing_type)
+    {
+        this.currently_playing_type = currently_playing_type;
+    }
+
+    public Actions getActions()
+    {
+        return actions;
+    }
+
+    public void setActions(Actions actions)
+    {
+        this.actions = actions;
+    }
+}
