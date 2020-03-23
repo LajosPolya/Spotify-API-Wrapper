8d219e472ed00a24189b4b66f4ab69138d0dbbbd
LajosPolya <lajospolya94@gmail.com>
2020-03-22 22:16:18 -0400

    GET Episode

---
+++ src/main/java/com/lajospolya/spotifyapiwrapper/request/GetEpisode.java
@@ -0,0 +1,50 @@
+package com.lajospolya.spotifyapiwrapper.request;
+
+import com.lajospolya.spotifyapiwrapper.response.Episode;
+
+import java.net.http.HttpRequest;
+
+public class GetEpisode extends AbstractSpotifyRequest<Episode>
+{
+    private static final String REQUEST_URI_STRING = SPOTIFY_V1_API_URI +  "episodes/{id}";
+
+    private GetEpisode(HttpRequest.Builder requestBuilder)
+    {
+        super(requestBuilder);
+    }
+
+    public static class Builder extends AbstractBuilder<GetEpisode>
+    {
+        private String episodeId;
+
+        private String market;
+
+        public Builder(String episodeId)
+        {
+            spotifyRequestParamValidationService.validateParametersNotNull(episodeId);
+            this.episodeId = episodeId;
+        }
+
+        @Override
+        public GetEpisode build()
+        {
+            SpotifyRequestBuilder spotifyRequestBuilder = new SpotifyRequestBuilder(REQUEST_URI_STRING, episodeId);
+            addOptionalQueryParams(spotifyRequestBuilder);
+            return new GetEpisode(spotifyRequestBuilder.createGetRequests());
+        }
+
+        private void addOptionalQueryParams(SpotifyRequestBuilder requestUriBuilder)
+        {
+            if(market != null)
+            {
+                requestUriBuilder.queryParam(MARKET_QUERY_PARAM, market);
+            }
+        }
+
+        public Builder market(String market)
+        {
+            this.market = market;
+            return this;
+        }
+    }
+}
