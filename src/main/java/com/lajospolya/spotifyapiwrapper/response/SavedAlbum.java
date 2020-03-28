fbffd215f39a0cada5af571658bad4501d327f0a
LajosPolya <lajospolya94@gmail.com>
2020-03-28 19:06:01 -0400

    getters and settes for SavedAlbum

--- src/main/java/com/lajospolya/spotifyapiwrapper/response/SavedAlbum.java
+++ src/main/java/com/lajospolya/spotifyapiwrapper/response/SavedAlbum.java
@@ -4,4 +4,24 @@ public class SavedAlbum
 {
     private String added_at;
     private Album album;
+
+    public String getAdded_at()
+    {
+        return added_at;
+    }
+
+    public void setAdded_at(String added_at)
+    {
+        this.added_at = added_at;
+    }
+
+    public Album getAlbum()
+    {
+        return album;
+    }
+
+    public void setAlbum(Album album)
+    {
+        this.album = album;
+    }
 }
